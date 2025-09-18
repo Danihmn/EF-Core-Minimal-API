eeac6e0885c34c2733af425c680bfe8c15c29c51
Danihmn <daniel.bezerra.mult@gmail.com>
2025-09-17 21:37:30 -0300

    refactor: Cria classe para ServicesExtensions, para separar classes em arquivos diferentes e deixar a classe Program limpa

--- Usings.cs
+++ Usings.cs
@@ -5,3 +5,4 @@ global using Perfumes.WebAPI.Contexto;
 global using Perfumes.WebAPI.Endpoints;
 global using Perfumes.WebAPI.Entidades;
 global using Perfumes.WebAPI.Endpoints.Services;
+global using Perfumes.WebAPI.Configuration;
